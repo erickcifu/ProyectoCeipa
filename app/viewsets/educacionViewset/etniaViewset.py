561f3c8f9fdfd25d653df3c296afb3cd2bff8229
erickcifu <erickalfred29@gmail.com>
2021-09-13 18:20:12 -0600

    viewsets

---
+++ app/viewsets/educacionViewset/etniaViewset.py
@@ -0,0 +1,14 @@
+import json
+from django.core.files import File
+from django_filters.rest_framework import djangoFilterBackend
+from rest_framework import status, filters, viewsets
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.settings import api_settings
+
+from app.models.educacion_model import ocupacion
+from app.serializer.educacionSerializer import ocupacionRegistroSerializer, ocupacionSerializer
+#adsdiasdas
