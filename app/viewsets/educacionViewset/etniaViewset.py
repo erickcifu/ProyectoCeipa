753ed27a9f9b9e4a00812c342c60c3ba558980a0
erickcifu <erickalfred29@gmail.com>
2021-09-13 18:42:25 -0600

    arreglo

--- app/viewsets/educacionViewset/etniaViewset.py
+++ app/viewsets/educacionViewset/etniaViewset.py
@@ -9,9 +9,4 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
 
-from app.models.educacion_model import ocupacion
-from app.serializer.educacionSerializer import ocupacionRegistroSerializer, ocupacionSerializer
-<<<<<<< HEAD
-=======
-#adsdiasdas
->>>>>>> 67230c60d8d899aab60e997b6c1ee50c01e41f1b
+#
