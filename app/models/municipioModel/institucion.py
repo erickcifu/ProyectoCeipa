7d1fe519bc09c77d27d1bf894c1caa00fb999c33
mafer85 <mendozamafer2000@gmail.com>
2021-09-24 23:15:35 -0600

    models que no se guardaron

---
+++ app/models/municipioModel/institucion.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Institucion(models.Model):
+    nombre_ins = models.CharField(max_length=255,null=False)
+    correo = models.EmailField(max_length=55, null=True, blank=True)
+    estado_ins = models.BooleanField(default=True)
+
+    def delete(self, *args):
+        self.estado_ins = False
+        self.save()
+        return True
