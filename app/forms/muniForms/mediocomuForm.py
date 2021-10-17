f08bf8e6b0838791913eb9fd833fd4081a4e8db4
erickcifu <erickalfred29@gmail.com>
2021-10-16 19:03:41 -0600

    Wizard de registro medios de comunicacion

--- app/forms/muniForms/mediocomuForm.py
+++ app/forms/muniForms/mediocomuForm.py
@@ -2,18 +2,14 @@ from django import forms
 from app.models import MedioComuni, Persona, Cargo
 
 class MedioComuniForm(forms.ModelForm):
-    persona = forms.ModelChoiceField(
-        queryset = Persona.objects.filter(estado_persona=True)
-        .order_by('persona')
-    )
     cargo = forms.ModelChoiceField(
         queryset = Cargo.objects.filter(estado_cargo=True)
         .order_by('nombre_cargo')
     )
     class Meta:
         model = MedioComuni
-        fields = ['nombre_medio', 'correo', 'telefono', 'persona', 'cargo', 'estado']
-        labels = {'nombre_medio':'Nombres', 'correo':'Correo', 'telefono':"Telefono", 'persona':'Persona', 'cargo':'Cargo', 'estado':'Estado'}
+        fields = ['nombre_medio', 'correo', 'telefono', 'cargo', 'estado']
+        labels = {'nombre_medio':'Nombres de el medio', 'correo':'Correo', 'telefono':"Telefono",  'cargo':'Cargo', 'estado':'Estado'}
         widget = {'nombre_medio', forms.TextInput}
 
     def __init__(self, *args, **kwargs):
@@ -22,5 +18,4 @@ class MedioComuniForm(forms.ModelForm):
             self.fields[field].widget.attrs.update({
                 'class':'form-control'
             })
-        self.fields['persona'].empty_label = "Seleccione Persona"
         self.fields['cargo'].empty_label = "Seleccione Cargo"
